870b6c762e77c45487d6afd91dd92e8640d6ed46
Mat Talma <p2pcarleton@gmail.com>
2017-02-04 23:35:50 -0500

    Added Transaction Class

---
+++ Models/Transaction.cs
@@ -0,0 +1,9 @@
+namespace GoblinPF.Models
+{
+    public class Transaction
+    {
+        public double Cost { get; set; }
+        public bool BuySell { get; set; }
+        public string Category { get; set; }
+    }
+}
