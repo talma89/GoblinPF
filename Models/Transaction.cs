a709b5566f5390355895f830ff1dfcc74c9dbbdb
Mat Talma <p2pcarleton@gmail.com>
2017-02-04 23:43:29 -0500

    added Id to Transaction

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -2,6 +2,7 @@ namespace GoblinPF.Models
 {
     public class Transaction
     {
+        public int Id { get; set; }
         public double Cost { get; set; }
         public bool BuySell { get; set; }
         public string Category { get; set; }
